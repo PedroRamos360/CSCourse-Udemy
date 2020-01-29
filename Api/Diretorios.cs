1b6da91883b62effd105d6e41337d3c98cc496dc
Pedro Ramos <ramosct70@hotmail.com>
2020-01-29 14:31:58 -0300

    Atualização

---
+++ Api/Diretorios.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+
+namespace CursoCSharp.Api {
+    class Diretorios {
+        public static void Executar() {
+            var novoDir = @"~/PastaCSharp".ParseHome();
+            var novoDirDestino = @"~/PastaCSharpDestino".ParseHome();
+            var dirProjeto = @"C:\Users\User\source\repos\CursoCSharp\CursoCSharp";
+            
+            if (Directory.Exists(novoDir)) {
+                Directory.Delete(novoDir, true); // true para excluir de forma recursiva
+            }
+
+            if (Directory.Exists(novoDirDestino)) {
+                Directory.Delete(novoDirDestino, true);
+            }
+
+            Directory.CreateDirectory(novoDir);
+            Console.WriteLine(Directory.GetCreationTime(novoDir));
+
+            Console.WriteLine("=============== Pastas ===============");
+            var pastas = Directory.GetDirectories(dirProjeto);
+            foreach (var pasta in pastas)
+                Console.WriteLine(pasta);
+
+            Console.WriteLine("\n\n=============== Arquivos ===============");
+            var arquivos = Directory.GetFiles(dirProjeto);
+            foreach (var arquivo in arquivos)
+                Console.WriteLine(arquivo);
+
+            Console.WriteLine("\n\n=============== Raíz ===============");
+            Console.WriteLine(Directory.GetDirectoryRoot(dirProjeto));
+
+            Directory.Move(novoDir, novoDirDestino);
+        }
+    }
+}
---
+++ Api/ExemploDirectoryInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CursoCSharp.Api {
+    class ExemploDirectoryInfo {
+        public static void Executar() {
+            var dirProjeto = @"C:\Users\User\source\repos\CursoCSharp\CursoCSharp";
+
+            var dirInfo = new DirectoryInfo(dirProjeto);
+
+            if (!dirInfo.Exists) {
+                dirInfo.Create();
+            }
+
+            Console.WriteLine("=============== Arquivos ===============");
+            var arquivos = dirInfo.GetFiles();
+            foreach (var arquivo in arquivos) {
+                Console.WriteLine(arquivo);
+            }
+
+            Console.WriteLine("\n=============== Pastas ===============");
+            var pastas = dirInfo.GetDirectories();
+            foreach (var pasta in pastas) {
+                Console.WriteLine(pasta);
+            }
+
+            Console.WriteLine(dirInfo.CreationTime);
+            Console.WriteLine(dirInfo.FullName);
+            Console.WriteLine(dirInfo.Root);
+            Console.WriteLine(dirInfo.Parent.Parent);
+        }
+    }
+}
